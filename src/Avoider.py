e31864032000e4503d1f63c0220cb69c72d14b80
johanth3108 <johanth6600@gmail.com>
2024-05-11 09:19:22 +0100

    final changes

--- src/Avoider.py
+++ src/Avoider.py
@@ -1,108 +1,269 @@
 #!/usr/bin/env python3
 
-class Avoider():
-	''' This class provides simple obstacle avoidance functionalities to a ROS robot '''
+import rospy 
+import math
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from sensor_msgs.msg import LaserScan
 
-	# This dict keeps track of the distance measures for each region
-	Regions_Report = {
-	                     "front_C": [], "front_L": [], "left_R" : [],
-	                     "left_C" : [], "left_L" : [], "back_R" : [],
-	                     "back_C" : [], "back_L" : [], "right_R": [],
-	                     "right_C": [], "right_L": [], "front_R": [],
-	                 }
-	# These are the costs to deviate from each region to the goal region (front_C)
-	Regions_Distances = {
-	                     "front_C":  0, "front_L":  1, "left_R" :  2,
-	                     "left_C" :  3, "left_L" :  4, "back_R" :  5,
-	                     "back_C" :  6, "back_L" : -5, "right_R": -4,
-	                     "right_C": -3, "right_L": -2, "front_R": -1,
-	                 	}
+class control(): 
 
-	def __init__(self, vel_obj, obstacle_threshold=0.7, 
-				       regional_angle=30, normal_lin_vel=0.4, 
-				       trans_lin_vel=-0.09, trans_ang_vel=1.75):
-		'''
-		:param vel_obj           : Velocity object; will contain velocity commands(data); Twist()
-		:param obstacle_threshold: Objects a this distance or below are considered obstacles
-		:param regional_angle    : The angle on which each region extends
-		:param normal_lin_vel    : When there's no obstacles, the robot will move with this linear velocity
-		:param trans_lin_vel     : After detecting an obstacle, the robot will back up (negative) 
-								   while rotating to help in case it can't perform a stationary rotation
-		:param trans_ang_vel 	 : The robot always rotates with the same value of angular velocity
-		'''
-		self.vel_obj        = vel_obj
-		self.OBSTACLE_DIST  = obstacle_threshold
-		self.REGIONAL_ANGLE = regional_angle
-		self.NORMAL_LIN_VEL = normal_lin_vel
-		self.TRANS_LIN_VEL  = trans_lin_vel
-		self.TRANS_ANG_VEL  = trans_ang_vel
+    def angle_callback(self, topic_data: Odometry):
+        pose = topic_data.pose.pose
+        orientation = pose.orientation 
+        (roll, pitch, yaw) = euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w], 'sxyz')
+        self.ori_z = yaw
 
-	def indentify_regions(self, scan):
-		'''
-		:param scan: Scan object that contains the lidar data 
-		'''
+        position = pose.position
+        self.pos_x = position.x
+        self.pos_y = position.y 
 
-		# This list keeps track of the order in which the regions' readings are obtained
-		REGIONS = [
-		             "front_C", "front_L", "left_R" ,
-		             "left_C" , "left_L" , "back_R" ,
-		             "back_C" , "back_L" , "right_R",
-		             "right_C", "right_L", "front_R",
-				  ]
+    def dist_callback(self, topic_data: LaserScan): 
+        dist = topic_data.ranges
+        dist = list(dist)
+        # Zero remover (for the actual thing)
+        for i in dist:
+            if i == 0:
+                dist[dist.index(i)] = 2
+                
+        self.lidar_data = dist
+        front_dist = dist[0:45]+dist[315:359]
+        left_dist = dist[45:135]
+        right_dist = dist[225:315]
+        self.min_front_dist = min(front_dist)
+        self.min_left_dist = min(left_dist)
+        self.min_right_dist = min(right_dist)
+        
+        back_dist = dist[135:225]
+        tilt_dist_left = dist[20:90]
+        tilt_dist_right = dist[270:340]
+        self.min_back_dist = min(back_dist)
+        self.min_tilt_dist_left = min(tilt_dist_left)
+        self.min_tilt_dist_right = min(tilt_dist_right)
 
-		# The front central region necessitate getting the last and first 15 points of the ranges
-		intermediary = scan.ranges[:int(self.REGIONAL_ANGLE/2)]\
-					 + scan.ranges[(len(scan.ranges)-1)*int(self.REGIONAL_ANGLE/2):]
-		self.Regions_Report["front_C"] = [x for x in intermediary if x <= self.OBSTACLE_DIST and x != 'inf']
-		
-		# Enumerate all regions but the first
-		for i, region in enumerate(REGIONS[1:]):
-			# Only objects at a distance less than or equal to the threshold are considered obstacles
-			self.Regions_Report[region] = [x for x in scan.ranges[self.REGIONAL_ANGLE*i:self.REGIONAL_ANGLE*(i+1)]\
-												   if x <= self.OBSTACLE_DIST and x != 'inf']
+    def order_callback(self, topic_data: Float64):
+        self.wanted_z = topic_data.data
+        #print(f"wanted_z = {self.wanted_z:.3f}")  
+        #print(f"ori_z = {self.ori_z:.3f}, wanted_z = {self.wanted_z:.3f}") 
 
-	def avoid(self):
-		act, ang_vel = self._clearance_test()
-		# If act is False, and_vel is set to 0
-		self._steer(act, (act*ang_vel))
-		return self.vel_obj
+    def __init__(self): 
+        self.node_name = "Controller" 
+        pub_topic_name = "cmd_vel" 
+        sub_topic_name1 = "odom"
+        sub_topic_name2 = "scan"
+        # sub_topic_name3 = "move_cmd"
 
-	def _clearance_test(self):
+        self.adj_side_dist = 0.2
+        self.normal_speed = 0.2
+        self.tilt_speed = 1
+        self.min_back_dist = 0
+        self.min_tilt_dist_left = 0
+        self.min_tilt_dist_right = 0
 
-		goal = "front_C"
-		closest = 10e6
-		regional_dist = 0
-		maxima = {"destination": "back_C", "distance": 10e-6}
-		for region in self.Regions_Report.items():
-			regional_dist = abs(self.Regions_Distances[region[0]]-self.Regions_Distances[goal])
-			#if there're no obstacles in that region
-			if not len(region[1]):
-				#check if it's the cheapest option
-				if (regional_dist < closest):
-					closest = regional_dist
-					maxima["distance"] = self.OBSTACLE_DIST
-					maxima["destination"] = region[0]
-			#check if it's the clearest option
-			elif(max(region[1]) > maxima["distance"]):
-				maxima["distance"] = max(region[1])
-				maxima["destination"] = region[0]
-		#calculate the cost to the chosen orientation
-		regional_dist = self.Regions_Distances[maxima["destination"]]-self.Regions_Distances[goal]
-		
-		# Return whether to act or not, and the angular velocity with the appropriate sign
-		return (closest != 0), (regional_dist/[abs(regional_dist) if regional_dist != 0 else 1][0])*self.TRANS_ANG_VEL
+        # Variable Defenition
+        self.ctrl_c = False
+        self.ori_z = 0
+        self.pos_x = 0
+        self.pos_y = 0
+        self.min_front_dist = 0
+        self.min_right_dist = 0
+        self.min_left_dist = 0
+        self.stop_dist = 0.37
+        self.obst_start_left_angle = 400
+        self.obst_end_left_angle = 400
+        self.obst_start_right_angle = 400
+        self.obst_end_right_angle = 400
+        self.left_obst_flag = False
+        self.right_obst_flag = False
+        self.left_angle_misplacement = 0
+        self.right_angle_misplacement = 0
 
-	def _steer(self, steer=False, ang_vel=0):
-		'''
-		:param steer  : Whether to avoid and obstacle or keep on going straigt
-		:param ang_vel: The angular velocity of the robot
-		'''
-		if not steer:
-			self.vel_obj.linear.x = self.NORMAL_LIN_VEL
-		else:
-			self.vel_obj.linear.x = self.TRANS_LIN_VEL
-		self.vel_obj.linear.y  = 0
-		self.vel_obj.linear.z  = 0
-		self.vel_obj.angular.x = 0
-		self.vel_obj.angular.y = 0
-		self.vel_obj.angular.z = ang_vel
+        self.lidar_data = []
+        self.revered_lidar_data = []
+        self.obj_loc = 0                # 0: No Object , 1: Front , 2: Right , 3: Left , 4: Front Right , 5: Front Left , 6 : Right Left     
+        self.prev_obj_loc = 0                # 0: No Object , 1: Front , 2: Right , 3: Left , 4: Front Right , 5: Front Left , 6 : Right Left    
+        self.prev_angle = 0  
+        self.RL_dif = 0 
+
+        rospy.init_node(self.node_name, anonymous=True) 
+
+        self.pub = rospy.Publisher(pub_topic_name, Twist, queue_size=10) 
+        self.sub1 = rospy.Subscriber(sub_topic_name1, Odometry, self.angle_callback)
+        self.sub2 = rospy.Subscriber(sub_topic_name2, LaserScan, self.dist_callback)
+        # self.sub3 = rospy.Subscriber(sub_topic_name3, Float64, self.order_callback)
+        self.rate = rospy.Rate(10) 
+
+        rospy.on_shutdown(self.shutdownhook) 
+
+        rospy.loginfo(f"The '{self.node_name}' node is active...") 
+
+    def shutdownhook(self): 
+        print(f"Stopping the '{self.node_name}' node at: {rospy.get_time()}")
+        stop_cmd = Twist()
+        stop_cmd.linear.x = 0
+        stop_cmd.angular.z = 0
+        self.pub.publish(stop_cmd)
+        self.ctrl_c = True
+
+    def main_loop(self):
+        while not self.ctrl_c: 
+
+            # out_angle = Float64()
+            vel_cmd = Twist()
+            out_angle = 0
+            self.prev_obj_loc = self.obj_loc
+            #### Calculates what angle the object is from right and left
+            # Left front obstacle angle detection 
+            for i in self.lidar_data[0:45]:
+                if self.left_obst_flag == False: 
+                    if i < self.stop_dist:
+                        self.obst_start_left_angle = self.lidar_data[0:45].index(i)
+                        i = i + 1
+                        self.left_obst_flag = True
+                    else: 
+                        self.obst_start_left_angle = 400
+                        self.obst_end_left_angle = 400
+
+                else:
+                    if i > self.stop_dist:
+                        self.obst_end_left_angle = self.lidar_data[0:45].index(i)
+                        if self.obst_end_left_angle == 0:
+                            self.obst_end_left_angle = 400
+                        self.left_obst_flag = False
+                        break
+
+            # print(f"LSA = {self.obst_start_left_angle:.3f}")
+            # print(f"LEA= {self.obst_end_left_angle:.3f}")
+
+            # Right front obstacle angle detection 
+            self.revered_lidar_data = self.lidar_data[::-1]
+            for i in self.revered_lidar_data[0:45]:
+                if self.right_obst_flag == False: 
+                    if i < self.stop_dist:
+                        self.obst_start_right_angle = self.revered_lidar_data[0:45].index(i)
+                        i = i - 1
+                        self.right_obst_flag = True
+                    else: 
+                        self.obst_start_right_angle = 400
+                        self.obst_end_right_angle = 400
+
+                else:
+                    if i > self.stop_dist:
+                        self.obst_end_right_angle = self.revered_lidar_data[0:45].index(i)
+                        if self.obst_end_right_angle == 0:
+                            self.obst_end_right_angle = 400
+                        self.right_obst_flag = False
+                        break
+            
+            # print("========================================")
+            # print(f"RSA = {self.obst_start_right_angle:.3f}")
+            # print(f"REA= {self.obst_end_right_angle:.3f}")
+            # print("========================================")
+
+            self.right_angle_misplacement = self.obst_end_right_angle - self.obst_start_right_angle
+            self.left_angle_misplacement = self.obst_end_left_angle - self.obst_start_left_angle
+            self.RL_dif = abs(self.right_angle_misplacement - self.left_angle_misplacement)
+
+            # Object Detection
+            if self.min_front_dist <= self.stop_dist:
+                if self.min_right_dist <= self.stop_dist:
+                    self.obj_loc = 4
+                elif self.min_left_dist <= self.stop_dist:
+                    self.obj_loc = 5
+                else: 
+                    self.obj_loc = 1
+            elif self.min_right_dist <= self.stop_dist:
+                    self.obj_loc = 2
+            elif self.min_left_dist <= self.stop_dist:
+                    self.obj_loc = 3
+            else:
+                self.obj_loc = 0
+
+            # print(f"obj loc = {self.obj_loc:.3f}")
+
+            # Motion Handler
+            if self.obj_loc == 0:
+                out_angle = self.ori_z
+            elif self.obj_loc == 1:
+                if self.RL_dif > 5:
+                    if self.left_angle_misplacement < 80 and self.left_angle_misplacement > 5:
+                        out_angle = self.ori_z - math.radians(self.left_angle_misplacement/2)
+                    elif self.right_angle_misplacement < 80 and self.right_angle_misplacement > 5:
+                        if self.prev_angle < self.ori_z + math.radians(self.right_angle_misplacement/2):
+                            out_angle = self.ori_z + math.radians(self.right_angle_misplacement/2)
+                    else:
+                        if self.prev_angle > self.ori_z:
+                            out_angle = self.ori_z + math.pi/2
+                        else:
+                            out_angle = self.ori_z - math.pi/2
+                else:
+                    if self.prev_angle > self.ori_z:
+                        out_angle = self.ori_z + math.pi/2
+                    else:
+                        out_angle = self.ori_z - math.pi/2
+            elif self.obj_loc == 2:
+                # if self.prev_obj_loc != 4:
+                out_angle = self.ori_z
+            elif self.obj_loc == 3:
+                # if self.prev_obj_loc != 5:
+                out_angle = self.ori_z
+            elif self.obj_loc == 4:
+                if self.right_angle_misplacement > 80:
+                    out_angle = self.ori_z + math.pi/2
+                else:
+                    if self.prev_angle < self.ori_z + math.radians(self.right_angle_misplacement/2):
+                        out_angle = self.ori_z + math.radians(self.right_angle_misplacement/2)
+            elif self.obj_loc == 5:
+                if self.left_angle_misplacement > 80:
+                    out_angle = self.ori_z - math.pi/2
+                else:
+                    out_angle = self.ori_z - math.radians(self.left_angle_misplacement/2)
+            elif self.obj_loc == 6:
+                out_angle = self.ori_z
+
+            # old code from here
+            print(out_angle)
+            self.wanted_z = out_angle
+            # angle_dif1 = self.wanted_z - self.ori_z
+            angle_dif1 = self.wanted_z - self.ori_z
+            angle_dif2 = (0 - math.pi - self.ori_z) - (math.pi - self.wanted_z)
+
+            if abs(angle_dif1) <= abs(angle_dif2):
+                anglelar_speed = angle_dif1 # rad/s
+            else :
+                anglelar_speed = angle_dif2 # rad/s
+
+            linear_speed_dist_front_modif = self.normal_speed*math.exp((self.stop_dist - self.min_front_dist)*10)
+            if (abs(linear_speed_dist_front_modif) > (self.normal_speed*0.8)) and (abs(linear_speed_dist_front_modif) < (self.normal_speed*1.2)):
+                linear_speed_dist_front_modif = self.normal_speed
+
+            linear_speed_dist_back_modif = self.normal_speed*math.exp((self.stop_dist - self.min_back_dist)*10)
+            if (abs(linear_speed_dist_back_modif) > (self.normal_speed*0.8)) and (abs(linear_speed_dist_back_modif) < (self.normal_speed*1.2)):
+                linear_speed_dist_back_modif = self.normal_speed
+
+            anglelar_speed_left_modif = 0
+            if self.min_tilt_dist_left < self.adj_side_dist:
+                anglelar_speed_left_modif = self.tilt_speed*math.exp((self.adj_side_dist*0.75 - self.min_tilt_dist_left)*5)
+            if abs(anglelar_speed_left_modif) < (self.tilt_speed*0.2) :
+                anglelar_speed_left_modif = 0
+            anglelar_speed_right_modif = 0
+            if self.min_tilt_dist_right < self.adj_side_dist:
+                anglelar_speed_right_modif = self.tilt_speed*math.exp((self.adj_side_dist*0.75 - self.min_tilt_dist_right)*5)
+            if abs(anglelar_speed_right_modif) < (self.tilt_speed*0.2) :
+                anglelar_speed_right_modif = 0
+
+            vel_cmd.linear.x = self.normal_speed - linear_speed_dist_front_modif + linear_speed_dist_back_modif - (anglelar_speed/math.pi)*self.normal_speed # m/s
+            vel_cmd.angular.z = anglelar_speed*2 + anglelar_speed_right_modif - anglelar_speed_left_modif
+            vel_cmd.angular.z = vel_cmd.angular.z
+            # print(vel_cmd)
+            self.pub.publish(vel_cmd)
+            self.rate.sleep()
+
+if __name__ == '__main__': 
+    controller_instance = control() 
+    try:
+        controller_instance.main_loop() 
+    except rospy.ROSInterruptException:
+        pass
